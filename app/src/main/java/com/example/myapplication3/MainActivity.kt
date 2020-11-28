dbf7fd0b26c03d33c7ecf4d834d2ba51d03c3468
reza <mrseifi@gmail.com>
2020-11-27 20:04:46 -0500

    11/27/2020

--- app/src/main/java/com/example/myapplication3/MainActivity.kt
+++ app/src/main/java/com/example/myapplication3/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        button_bank1.
 
         val button1 = findViewById<Button>(R.id.button_bank1)
         button1.setOnClickListener {
